124099afd7cdcd34814ba78ed97e9cf2cad71348
StaticMaster <gnaboys@gmail.com>
2022-07-11 16:57:39 -0400

    changes to login and regristration code

--- lifetracker-ui/src/components/RegistrationForm/RegistrationForm.jsx
+++ lifetracker-ui/src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,10 +1,85 @@
 import React, { useState } from 'react'
 import "./RegistrationForm.css"
+import axios from "axios"
+
 export default function RegistrationForm() {
 
  function handleOnCreateAccount() {
-   const [isCreateAccount, createAccount] = useState(false);
-   
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false)
+  const [errors, setErrors] = useState({})
+  const [form, setForm] = useState({
+    email: "",
+    username: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+    passwordConfirm: "",
+  })
+
+  const handleOnInputChange = (event) => {
+    if (event.target.name === "password") {
+      if (form.passwordConfirm && form.passwordConfirm !== event.target.value) {
+        setErrors((e) => ({ ...e, passwordConfirm: "Password's do not match" }))
+      } else {
+        setErrors((e) => ({ ...e, passwordConfirm: null }))
+      }
+    }
+    if (event.target.name === "passwordConfirm") {
+      if (form.password && form.password !== event.target.value) {
+        setErrors((e) => ({ ...e, passwordConfirm: "Password's do not match" }))
+      } else {
+        setErrors((e) => ({ ...e, passwordConfirm: null }))
+      }
+    }
+    if (event.target.name === "email") {
+      if (event.target.value.indexOf("@") === -1) {
+        setErrors((e) => ({ ...e, email: "Please enter a valid email." }))
+      } else {
+        setErrors((e) => ({ ...e, email: null }))
+      }
+    }
+
+    setForm((f) => ({ ...f, [event.target.name]: event.target.value }))
+  }
+
+  const handleOnSubmit = async () => {
+    setIsLoading(true)
+    setErrors((e) => ({ ...e, form: null }))
+
+    if (form.passwordConfirm !== form.password) {
+      setErrors((e) => ({ ...e, passwordConfirm: "Passwords do not match." }))
+      setIsLoading(false)
+      return
+    } else {
+      setErrors((e) => ({ ...e, passwordConfirm: null }))
+    }
+
+    try {
+      const res = await axios.post("http://localhost:3001/auth/register", {
+        email: form.email,
+        password: form.password,
+        username: form.username,
+        firstName: form.firstName,
+        lastName: form.lastName,
+        
+      })
+
+      if (res?.data?.user) {
+        setAppState(res.data)
+        setIsLoading(false)
+        navigate("/portal")
+      } else {
+        setErrors((e) => ({ ...e, form: "Something went wrong with registration" }))
+        setIsLoading(false)
+      }
+    } catch (err) {
+      console.log(err)
+      const message = err?.response?.data?.error?.message
+      setErrors((e) => ({ ...e, form: message ? String(message) : String(err) }))
+      setIsLoading(false)
+    }
+  }
  }
 
   return (
@@ -39,7 +114,7 @@ export default function RegistrationForm() {
           <input type="password" name="passwordConfirm" placeholder="Confirm your password" className='form-input'></input><br></br>
         </div>
           </div>
-          <button type="button" onClick={handleOnCreateAccount} iscreateAccount ={()=>createAccount(false)} className='register-button'>Create Account</button> 
+          <button type="button" onClick={handleOnSubmit} className='register-button'>Create Account</button> 
         </form>   
         <div className='footer'>
         <p>
